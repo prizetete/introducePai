5e11cd72a939c489553f1a82d230237dda9fbad9
Komkrit Siratitanan <prizetete1212@gmail.com>
2019-03-29 14:04:02 +0700

    commit lastest

--- IntroducePaiApp/Controller/SettingController.swift
+++ IntroducePaiApp/Controller/SettingController.swift
@@ -8,31 +8,57 @@
 
 import UIKit
 
-class SettingController: UIViewController {
+class SettingController: UIViewController, UITextViewDelegate {
     
     private var mTableview: UITableView!
-    
+    private var mTextViewJa: textviewJa!
+    private var mTextViewJa2: textFieldJa!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavBarColor()
         
-        self.mTableview = UITableView()
-        self.mTableview.delegate = self
-        self.mTableview.dataSource = self
+        self.view.backgroundColor = .white
         
-        self.mTableview.translatesAutoresizingMaskIntoConstraints = false
-        self.mTableview.backgroundColor = .yellow
-        self.mTableview.register(SettingCell.self, forCellReuseIdentifier: "SettingCell")
+        self.mTextViewJa = textviewJa()
+        self.mTextViewJa2 = textFieldJa()
+//        self.mTextViewJa.delegate = self
+        self.view.addSubview(self.mTextViewJa)
+        self.mTextViewJa.translatesAutoresizingMaskIntoConstraints = false
+//        self.mTextViewJa.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8.0).isActive = true
+        self.mTextViewJa.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.mTextViewJa.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        self.mTextViewJa.widthAnchor.constraint(equalToConstant: 200.0).isActive = true
+        self.mTextViewJa.heightAnchor.constraint(equalToConstant: 30.0).isActive = true
         
-        self.view.addSubview(self.mTableview)
+        self.view.addSubview(self.mTextViewJa2)
+        self.mTextViewJa2.translatesAutoresizingMaskIntoConstraints = false
+        //        self.mTextViewJa.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8.0).isActive = true
+        self.mTextViewJa2.topAnchor.constraint(equalTo: self.mTextViewJa.bottomAnchor, constant: 16.0).isActive = true
+        self.mTextViewJa2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.mTextViewJa2.widthAnchor.constraint(equalToConstant: 300.0).isActive = true
+        self.mTextViewJa2.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
         
-        self.mTableview.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        self.mTableview.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.mTableview.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        self.mTableview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+//        self.mTableview = UITableView()
+//        self.mTableview.delegate = self
+//        self.mTableview.dataSource = self
+//
+//        self.mTableview.translatesAutoresizingMaskIntoConstraints = false
+//        self.mTableview.backgroundColor = .yellow
+//        self.mTableview.register(SettingCell.self, forCellReuseIdentifier: "SettingCell")
+//
+//        self.view.addSubview(self.mTableview)
+//
+//        self.mTableview.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+//        self.mTableview.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+//        self.mTableview.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+//        self.mTableview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
     }
+    
+//    func textViewDidChange(_ textView: UITextView) {
+//        print("change: \(textView.text)")
+//    }
     
 
 }
@@ -101,4 +127,120 @@ class SettingCell: UITableViewCell {
         self.mLabel.textAlignment = .center
     }
     
+}
+
+
+class textviewJa: UITextView {
+    var mView: UIView!
+    var mTextInLabel: UILabel!
+    
+    override init(frame: CGRect = .zero, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        self.delegate = self
+        self.layer.cornerRadius = 10.0
+        self.layer.borderColor = UIColor.gray.cgColor
+        self.layer.borderWidth = 2.0
+        self.backgroundColor = .white
+        self.mView = UIView()
+        self.mTextInLabel = UILabel()
+        self.mTextInLabel.text = "สวัสดีจ้า"
+        self.mTextInLabel.font = .systemFont(ofSize: 10.0)
+        self.mTextInLabel.textAlignment = .center
+        self.mTextInLabel.textColor = UIColor.red
+        self.mView.backgroundColor = .white
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension textviewJa: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print("begin editing")
+        
+        self.superview?.addSubview(self.mView)
+        self.mView.addSubview(self.mTextInLabel)
+        self.mView.translatesAutoresizingMaskIntoConstraints = false
+        self.mView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10.0).isActive = true
+        self.mView.heightAnchor.constraint(equalToConstant: 8.0).isActive = true
+        self.mView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+        self.mView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        self.mTextInLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.mTextInLabel.centerXAnchor.constraint(equalTo: self.mView.centerXAnchor).isActive = true
+        self.mTextInLabel.centerYAnchor.constraint(equalTo: self.mView.centerYAnchor).isActive = true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print("end editing")
+        self.mView.removeFromSuperview()
+    }
+}
+
+class textFieldJa: UITextField {
+    var mView: UIView!
+    var mTextInLabel: UILabel!
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        self.delegate = self
+        self.layer.cornerRadius = 10.0
+        self.layer.borderColor = UIColor.gray.cgColor
+        self.layer.borderWidth = 2.0
+
+        self.mView = UIView()
+        self.mTextInLabel = UILabel()
+        self.mTextInLabel.text = "สวัสดีจ้า"
+        self.mTextInLabel.font = .systemFont(ofSize: 10.0)
+        self.mTextInLabel.textAlignment = .center
+        self.mTextInLabel.textColor = UIColor.gray
+        self.mView.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+}
+
+extension textFieldJa: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("begin111")
+//        self.placeholder = ""
+        UIView.animate(withDuration: 0.35) {
+            self.superview?.addSubview(self.mView)
+            self.mView.addSubview(self.mTextInLabel)
+            self.mView.translatesAutoresizingMaskIntoConstraints = false
+            self.mView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 15.0).isActive = true
+            self.mView.heightAnchor.constraint(equalToConstant: 8.0).isActive = true
+            self.mView.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+            self.mView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            self.mTextInLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.mTextInLabel.centerXAnchor.constraint(equalTo: self.mView.centerXAnchor).isActive = true
+            self.mTextInLabel.centerYAnchor.constraint(equalTo: self.mView.centerYAnchor).isActive = true
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        print("end1111")
+//        self.placeholder = "สวัสดีจ้า"
+        self.mView.removeFromSuperview()
+    }
 }
